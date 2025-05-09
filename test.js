13a746e021ea4d1fd8a75192cbef3752a2904b96
andimags <andimagsumbol@gmail.com>
2025-05-09 18:43:56 +0800

    solved 6. zigzag conversion

--- test.js
+++ test.js
@@ -1,45 +1,78 @@
 /**
- * @param {number} num
+ * @param {string} s
+ * @param {number} numRows
  * @return {string}
  */
-var intToRoman = function(num) {
-    romanNumerals = [
-        ['I', 1],
-        ['IV', 4],
-        ['V', 5],
-        ['IX', 9],
-        ['X', 10],
-        ['XL', 40],
-        ['L', 50],
-        ['XC', 90],
-        ['C', 100],
-        ['CD', 400],
-        ['D', 500],
-        ['CM', 900],
-        ['M', 1000]
-    ];
+var convert = function(s, numRows) {
+    let row = 0;
+    let col = 0;
+    let row_to_inc = true;
+    let row_cols_to_insert = [];
+    
+    for(let i = 0; i < s.length; i++){
+        row_cols_to_insert.push([row, col]);
 
-    let roman = '';
-    let remaining = num;
-
-    // console.log('remaining: ', remaining, 'roman numerals: ', roman);
-
-    for(let i = romanNumerals.length - 1; i >= 0; i--){
-        if(remaining == 0){
-            break;
+        if(numRows == 1){
+            col++;
+            continue;
         }
 
-        while(remaining >= romanNumerals[i][1]){
-            roman += romanNumerals[i][0];
-            remaining -= romanNumerals[i][1];
+        if(row == numRows - 1){
+            row_to_inc = false;
+        }
 
-            // console.log('remaining: ', remaining, 'roman numerals: ', roman);
+        if(row == 0){
+            row_to_inc = true;
+        }
+
+        if(row_to_inc){
+            row++;
+        }
+        else{
+            row--
+            col++;
         }
     }
 
-    return roman;
+    // Create empty array
+    let zigzag_array = [];
+
+    // console.log(row_cols_to_insert);
+    // console.log(row_cols_to_insert[row_cols_to_insert.length - 1]);
+    console.log(numRows, row_cols_to_insert[row_cols_to_insert.length - 1][1]);
+
+    for(let i = 0; i < numRows; i++){
+        for(let j = 0; j <= row_cols_to_insert[row_cols_to_insert.length - 1][1]; j++){
+            if(zigzag_array[i] == undefined){
+                zigzag_array.push([]);
+            }
+
+            zigzag_array[i].push('');
+        }
+    }
+
+    console.table(zigzag_array);
+
+    // Insert letters on appropriate indices
+    for(let i = 0; i < row_cols_to_insert.length; i++){
+        zigzag_array[row_cols_to_insert[i][0]][row_cols_to_insert[i][1]] = s[i];
+    }
+
+    let reversed = "";
+
+    for(let i = 0; i < zigzag_array.length; i++){
+        for(let j = 0; j < zigzag_array[i].length; j++){
+            if(zigzag_array[i][j] != ''){
+                reversed += zigzag_array[i][j];
+            }
+        }
+    }
+
+    return reversed;
 };
 
-console.log(intToRoman(3749)); // MMMDCCXLIX
-console.log(intToRoman(58)); // LVIII
-console.log(intToRoman(1994)); // MCMXCIV
+// console.log(convert('PAYPALISHIRING', 3));
+// console.log(convert('PAYPALISHIRING', 4));
+// console.log('reversed: ', convert('AB', 1));
+// console.log('reversed: ', convert('ABC', 1));
+console.log('reversed: ', convert('ABCD', 1));
